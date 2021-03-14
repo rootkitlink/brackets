fcb30903f51913b9279593345a68ac015cefdef7
Sergei Goncharov <rootkitlink@gmail.com>
2021-03-14 18:17:03 +0200

    fix: made working function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = str.length;
+  for (let n = 0; n < i; n++){
+    for (let m = 0; m < bracketsConfig.length; m++){
+      let brackets = bracketsConfig[m][0] + bracketsConfig[m][1];
+      str.includes(brackets) ? str = str.replace(brackets, '') : str = str;
+    }
+  }
+  return str == '' ? true : false;
+
+  // let stack = [], openBr = [], closeBr = [];
+  // if (str.length <= 1) return false;
+  // for (let i = 0; i < bracketsConfig.length; i++){
+  //   (i % 2 === 0) ? openBr.push(bracketsConfig[i]) : closeBr.push(bracketsConfig[i]);
+  // }
+  // openBr = openBr.join('');
+  // closeBr = closeBr.join('');
+  // for (i = 0; i < str.length; i++){
+  //   if (closeBr.indexOf(str[i]) > -1){
+  //     if (stack.length == 0 || (stack.pop() != openBr[closeBr.indexOf(str[i])])){
+  //       return false;
+  //     }
+  //   }
+  //   else {
+  //       stack.push(str[i]);
+  //     }
+  // }
+  // return (stack.length == 0);
 }
